cc4439a1d4d109fb5eaae15d6a16e824b04ebb6b
HasanDevops <98355592+HasanDevops@users.noreply.github.com>
2024-06-16 00:28:05 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,11 @@
 variable "aws_access_key_id" {
   description = "AWS access key"
   type        = string
+  sensitive   = true
 }
 
 variable "aws_secret_access_key" {
   description = "AWS secret key"
   type        = string
-}
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  sensitive   = true
 }
